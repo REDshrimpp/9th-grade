074d2d16a09407bb5fe21fcce8b785dcb43fec97
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-09-08 14:59:06 -0700

    Updating assignments/number-functions/code.js

--- assignments/number-functions/code.js
+++ assignments/number-functions/code.js
@@ -27,3 +27,5 @@ const averageOfThree = (a,b,c) => (a+b+c)/3
 const distance = (a,b) => (Math.abs(a-b))
 
 const manhattanDistance = (a,b,c,d) => (Math.abs(a-c)+Math.abs(b-d))
+
+const euclideanDistance = (a,b,c,d) => (Math.sqrt((Math.abs(a,c)+Math.abs(b,d)**2))
