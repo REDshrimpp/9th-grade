b345d1b9f1de409f85c24ed7113c2c87d3c0d799
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-09-09 15:09:18 -0700

    Updating assignments/boolean-birds/code.js

--- assignments/boolean-birds/code.js
+++ assignments/boolean-birds/code.js
@@ -27,3 +27,5 @@ const isGlobbyBird = (isRed,isSpotted) => !isRed&&!isSpotted
 const eatsWorms = (isRed,isSpotted) => isRed||isSpotted
 
 const eatsNuts = (isRed,isSpotted) => isRed||!isSpotted
+
+const eatsFish = (isRed,isSpotted) => isSpotted||!isRed
