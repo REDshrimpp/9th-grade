9e69af408880a780d0af43365714846a464540c5
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-09-30 15:19:53 -0700

    Updating assessments/unit-01/code.js

--- assessments/unit-01/code.js
+++ assessments/unit-01/code.js
@@ -5,3 +5,52 @@ const emit = (a, b, product) => {
 };
 
 // Write your code here ...
+
+//1
+const averageWeight = (a, b) => {
+  a / b
+}
+
+//2
+const hypotenuse = (a, b) => {
+  a ** 2 + b ** 2
+}
+
+//3
+const maxRadius = (a, b) => {
+  Math.min(a, b) / 2
+}
+
+//4
+const numCircles = (a, b) => {
+  b / 2 * a
+}
+
+//5
+const offset = (a,b) => {
+  (b - a) / 2
+}
+
+//6
+const canSleepIn = (a,b) => {
+  !a || b === true
+} 
+
+//7
+const canGoToProm = (a,b,c) => {
+  a || b && !c === true
+}
+
+//8
+const getsSpeedingTicket = (a,b) => {
+  if (a>70) {
+    b || !b === true
+  }
+  else if (70 > a > 65) {
+    b === true
+  }
+  else
+  b || !b === false
+}
+
+//9
