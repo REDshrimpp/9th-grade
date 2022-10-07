3ec280f17dfeea11e23d4e7db6078c03e5001cb2
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-10-07 14:58:12 -0700

    Updating assessments/unit-01-numeric/code.js

--- assessments/unit-01-numeric/code.js
+++ assessments/unit-01-numeric/code.js
@@ -7,3 +7,33 @@ const G = 6.6743e-11;
 // syntactically correct function skeleton, more credit for a skeleton with a
 // reasonable argument list, yet more credit for a working function, and full
 // credit for a clear and simple working function.
+
+//1
+const itemsLeftOver = (numberOfPeople, numberOfItems) => {
+  return numberOfItems % numberOfPeople
+}
+
+//2
+const areaOfCircle = (radius) => {
+  return Math.PI * (radius ** 2)
+}
+
+//3
+const volumeOfCube = (edgeLength) => {
+  return edgeLength ** 3
+}
+
+//4
+const populationGrowth = (currentPopulation, growthRate) => {
+  return currentPopulation + currentPopulation * growthRate
+}
+
+//5
+const earnedRunAverage = (numberOfEarnedRuns, numberOfInningsPitched) => {
+  return numberOfEarnedRuns / numberOfInningsPitched * 9
+}
+
+//6
+const valueOfJewels = (numberOfDiamonds, numberOfEmeralds, diamondValue, emeraldValue) => {
+  return numberOfDiamonds * diamondValue + numberOfEmeralds * emeraldValue
+}
