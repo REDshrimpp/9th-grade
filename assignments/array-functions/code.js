c850fe99cc228ca0fca9c09ec2a2129238a74e9d
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-10-13 15:14:30 -0700

    Updating assignments/array-functions/code.js

--- assignments/array-functions/code.js
+++ assignments/array-functions/code.js
@@ -1,7 +1,7 @@
 const countTens = (array) => {
   let y = 0
   let amountOfTens = 0
-  for (x = 0; x < array.length; x = x + 1) {
+  for (let x = 0; x < array.length; x = x + 1) {
     if (array[y] === 10) {
       amountOfTens++
     }
