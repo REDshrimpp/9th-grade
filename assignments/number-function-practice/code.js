27eecd6f4cff0073ffed6a02a9417a9e0df4f81a
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-10-18 15:22:08 -0700

    Updating assignments/number-function-practice/code.js

--- assignments/number-function-practice/code.js
+++ assignments/number-function-practice/code.js
@@ -1,7 +1,11 @@
+//sum
 const sum = (number1, number2) => {
   return number1 + number2
 }
 
+//averageOfThree
 const averageOfThree = (number1, number2, number3) => {
-  return (number1 + number2 + numbere3) / 3
+  return (number1 + number2 + number3) / 3
 }
+
+//
