bc6f61daa6d0d982a6455068d67a80562d35d6ef
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-10-19 15:22:55 -0700

    Updating assessments/unit-01-booleans-and-strings/code.js

--- assessments/unit-01-booleans-and-strings/code.js
+++ assessments/unit-01-booleans-and-strings/code.js
@@ -73,7 +73,7 @@ const randomCharacter = (string) => {
 
 //10
 const randomCharacterUpDown = (string) => {
-  let randomCharacter = string[rand(string.length)]
+  let randomCharacter = string/substring(rand(string.length))
   return randomCharacter + randomCharacter.toUpperCase
 }
 
