ee127ff302d82f394aede333d08f088e1f748d60
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-02 14:54:44 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -5,5 +5,9 @@ const findFnord = (string) => {
 
 //string contains
 const stringContains = (string1, string2) => {
-  return string1.search(/[string2]/)
+  if (string1.search(/[string2]/) !== -1) {
+    return true
+  }
+  else
+  return false
 }
