ef8dc20cf01caaccb73a45dbd7827406a4acfeba
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-02 14:58:01 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -1,6 +1,6 @@
 //findFnord
 const findFnord = (string) => {
-  return string.substring(3, string.length).search(/[Fnord]/)
+  return string.substring(2, string.length).search(/[Fnord]/)
 }
 
 //string contains
