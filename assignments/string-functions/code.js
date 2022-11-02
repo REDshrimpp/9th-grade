f18950208cb402a22bae411a114e1c54a9648477
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-02 15:08:59 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -15,6 +15,5 @@ const stringContains = (string1, string2) => {
 //first and last
 const firstAndLast = (string) => {
   if (string.length > 1)
-  return string[0] + string[length
-  ]
+  return string[0] + string[length]
 }
