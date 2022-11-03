db1416e77aea71cc1b05141d1f84c2c4d38b37ff
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-03 15:08:16 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -108,5 +108,5 @@ const lastThree = (string) => {
 
 //capitilize
 const capitalize = (string) => {
-  return string[0].toUpperCase() + string.substring(1,string.length)
+  return string[0].toUpperCase() + string.substring(1,string.length).toLowerCase()
 }
