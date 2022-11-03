a5a39b3b20e82bef429952322bd9d93e65b41c9c
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-03 14:44:04 -0700

    Updating assignments/string-functions/code.js

--- assignments/string-functions/code.js
+++ assignments/string-functions/code.js
@@ -41,3 +41,8 @@ const sameIgnoringCase = (string1, string2) => {
 const firstHalf = (string) => {
 return string.substring(0, string.length / 2)
 }
+
+//second half
+const secondHalf = (string) => {
+  return string.substring(string.length / 2, string.length)
+}
