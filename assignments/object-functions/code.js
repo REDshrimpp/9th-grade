d3e2e5c82fb0119b3bff240bdd59bf740277d646
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-09 15:30:19 -0800

    Updating assignments/object-functions/code.js

--- assignments/object-functions/code.js
+++ assignments/object-functions/code.js
@@ -17,3 +17,11 @@ const emptyObject = () => {
 const distance = (p1, p2) => {
   return Math.sqrt(Math.abs(p1.x - p2.x) **2 + Math.abs(p1.y - p2.y) **2)
 }
+
+//mid point
+const midpoint = (p1, p2) => {
+  let result = {x : 0, y : 0}
+  result.x = (p1.x + p2.x) / 2
+  result.y = (p1.y + p2.y) / 2
+  return result
+}
