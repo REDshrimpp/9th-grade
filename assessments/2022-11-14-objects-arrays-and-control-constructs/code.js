2e61a18389035bbd6b066e83ec82b1c56962cebe
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-14 15:07:23 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -14,7 +14,7 @@ const isSamePoint = (p1, p2) => {
 };
 
 const totalWithTip = (bill, tipPercentage) => {
-  let result = {subtotal:,tip:(), total: }
+  let result = {subtotal:bill.subtotal,tip:(bill.subtotal * tipPercentage), total: (bill + bill * subtotal)}
   return result
 };
 
