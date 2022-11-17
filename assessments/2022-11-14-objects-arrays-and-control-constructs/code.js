00e82acedd68cd27ee723947c8a3a5f99f184147
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-17 15:02:08 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -35,7 +35,7 @@ const bigWinners = (players) => {
 };
 
 const fillTimesTable = (table) => {
-  for (let n = 0; n < table.length; n++) {
+  for (let n = 1; n < table.length; n++) {
   for(let y = 0; y < table.length; y++) {
   for(let x = 0; x < table[y].length; x++) {
     table[y,x] = n * x
