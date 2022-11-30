41661520e18e56f52ca8cb08eca05e15af83a762
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-11-30 14:48:41 -0800

    Updating assignments/tic-tac-toe/code.js

--- assignments/tic-tac-toe/code.js
+++ assignments/tic-tac-toe/code.js
@@ -33,7 +33,7 @@ registerOnclick((x, y) => {
     }
     //middle left
     else
-      if ((y < height / 3 * 2 && x < width / 3) && xs[1][0] === 0) {
+      if ((y < height / 3 * 2 && y > height / 3 && x < width / 3) && xs[1][0] === 0) {
         xs[1][0] = currentShape
         drawText(currentShape, width * 1 / 6 - 50, height * 3 / 6 + 25, color, Math.min(width, height) * 0.3);
         clickCount++
@@ -80,5 +80,3 @@ const drawFrame = () => {
 }
 
 drawFrame()
-
-registerOnClick()
