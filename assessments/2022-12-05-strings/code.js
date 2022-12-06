a88beb0644e15d69e79948107f251397f0634dc4
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-12-06 15:13:51 -0800

    Updating assessments/2022-12-05-strings/code.js

--- assessments/2022-12-05-strings/code.js
+++ assessments/2022-12-05-strings/code.js
@@ -1,6 +1,6 @@
-//1
+//1 NOTE
 const upToX = (string) => {
-  return string.substring(0, string.search(/[x]/))
+  return string.substring(0, string.indexOf(/[x]/))
 }
 
 //2
@@ -11,4 +11,9 @@ const charactersAround = (string, index) => {
 //3
 const middle = (string) => {
   return string.substring(string.length / 4, string.length / 4 * 3)
+}
+
+//4
+const pair = (string1, string2) => {
+  return string1 + 'and' + string2
 }
