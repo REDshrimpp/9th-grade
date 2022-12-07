eb863bf34dfecb6252e37185b3f3ac70a8cb57d4
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-12-07 15:02:05 -0800

    Updating assessments/2022-12-07-control-constructs/code.js

--- assessments/2022-12-07-control-constructs/code.js
+++ assessments/2022-12-07-control-constructs/code.js
@@ -94,7 +94,7 @@ const logIfOk = (argument) => {
 //3 NOTE
 const timeToLeet = () => {
   let x = 0
-  for(let y = 0; !isLeet(x); y++) {
+  for(let y = 0; isLeet(x); y++) {
     x = random10k()
   }
   return x
