a9c98002e2fdf8cecd6325867e27be9d267271e4
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-12-13 15:19:42 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -1,27 +1,33 @@
 // For a change of pace, I'm providing you with all the function skeletons. This
 // should save you some time.
 
+//1
 const area = (rect) => {
   return rect.width * rect.height
 };
 
+//2
 const higherPaid = (e1, e2) => {
-  return {salary: Math.max(e1.salary,e2.salary)}
+  return Math.max(e1.salary,e2.salary)
 };
 
+//3
 const isSamePoint = (p1, p2) => {
   return p1.x === p2.x && p1.y === p2.y
 };
 
+//4
 const totalWithTip = (bill, tipPercentage) => {
   let result = { subtotal: bill.subtotal, tip: (bill.subtotal * tipPercentage), total: (tipPercentage * subtotal) }
   return result
 };
 
+//5
 const isWinner = (player) => {
   return player.score > 100
 };
 
+//6
 const updateWins = (players) => {
   for (let x = 0; x < players.length; x++) {
     if (isWinner(players[x])) {
@@ -31,6 +37,7 @@ const updateWins = (players) => {
   return players
 };
 
+//7
 const bigWinners = (players) => {
   let result = []
   for (let x = 0; x < players.length; x++) {
@@ -41,6 +48,7 @@ const bigWinners = (players) => {
   return result
 };
 
+//8
 const fillTimesTable = (table) => {
   for (let x = 0; x < table.length; x++) {
     for (let y = 0; y < table[x].length; y++) {
@@ -49,6 +57,7 @@ const fillTimesTable = (table) => {
   }
 };
 
+//9
 const sums = (n) => {
   let result = [0]
   for (let x = 0; x < n + 1; x++) {
@@ -57,6 +66,7 @@ const sums = (n) => {
   return result.shift
 };
 
+//10
 const rule110 = (cells) => {
   let result = []
   cells.unshift(0)
