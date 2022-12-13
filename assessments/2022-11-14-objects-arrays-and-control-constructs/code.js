21fdd344f36c1cb5495394261007332ed7af6f05
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-12-13 15:21:53 -0800

    Updating assessments/2022-11-14-objects-arrays-and-control-constructs/code.js

--- assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
+++ assessments/2022-11-14-objects-arrays-and-control-constructs/code.js
@@ -8,7 +8,12 @@ const area = (rect) => {
 
 //2
 const higherPaid = (e1, e2) => {
-  return Math.max(e1,e2)
+  if (e1.salary > e2.salary) {
+    return e1
+  }
+  else {
+    return e2
+  }
 };
 
 //3
