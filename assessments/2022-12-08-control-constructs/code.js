487202a519b6201fa828217e4c0b74c69377117e
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-12-13 15:15:25 -0800

    Updating assessments/2022-12-08-control-constructs/code.js

--- assessments/2022-12-08-control-constructs/code.js
+++ assessments/2022-12-08-control-constructs/code.js
@@ -23,8 +23,8 @@ const countXs = (string) => {
 
 //3 NOTE
 const timesTable = (number) => {
-  for (let x = 0; x < number + 1; x++) {
-    for (let y = 0; y < number + 1; y++) {
+  for (let x = 1; x < number + 1; x++) {
+    for (let y = 1; y < number + 1; y++) {
       emit(x, y, x * y)
     }
   }
