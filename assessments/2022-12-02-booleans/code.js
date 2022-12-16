f2690550afc0a7795321a68d781fec5d87248acf
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2022-12-16 10:54:03 -0800

    Updating assessments/2022-12-02-booleans/code.js

--- assessments/2022-12-02-booleans/code.js
+++ assessments/2022-12-02-booleans/code.js
@@ -9,33 +9,11 @@ const needSunscreen = (goingToBeach, goingSkiing) => {
 }
 
 //3
-const needMittens = (goingOutside, somewhereWarm) => {
-  return goingOutside && !somewhereWarm
+const needMittens = (goingOutside, someWhereWarm) => {
+  return goingOutside && !someWhereWarm
 }
 
 //4
 const isVenomous = (isStriped, hasBlueHead) => {
-return isStriped || !hasBlueHead
+  return isStriped || (!isStriped, !hasBlueHead)
 }
-
-//5
-const okaySpeed = (speedLimit, carSpeed) => {
-  return (carSpeed > speedLimit - 11) && (carSpeed < speedLimit + 11)
-}
-
-//6
-const twiceAsExpensive = (item1Price, item2Price) => {
-  return item1Price > item2Price * 2
-}
-
-//7
-const winningRecord = (wins, losses, ties) => {
-  return wins > losses + ties
-}
-
-//8
-const isMagicNumber = (number) => {
-  return number === 42 || number === 17
-}
-
-// 42 is the meaning of life
