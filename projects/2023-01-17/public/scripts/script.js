9c56e1ea9455e6f9612e52533d23c17e68bd3455
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-01-20 22:57:30 +0000

    wip

--- projects/2023-01-17/public/scripts/script.js
+++ projects/2023-01-17/public/scripts/script.js
@@ -3,15 +3,21 @@ const body = document.querySelector('body');
 
 // Create a new H1 element.
 const q1 = document.createElement('h1');
-const q2 = document.createElement('p');
+const p1 = document.createElement('p');
 
 // Create a text node and add it to the H1
 q1.append(document.createTextNode('Can you tell me why HTML was developed?'));
-q2.append(document.createTextNode('This is going to be the paragraph lets see if it works'));
+p1.append(document.createTextNode('This is going to be the paragraph lets see if it works'));
+//p1.append(document.)
 
 // Add the new H1 element to the body.
 body.append(q1);
-body.append(q2);
+body.append(p1);
+
+
+ol
+li
+
 
 // Create a new P element.
 const a1 = document.createElement('p');
