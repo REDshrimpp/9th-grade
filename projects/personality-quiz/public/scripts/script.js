519e11598cd7b7147d71a982d00e5eb959204b99
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-01-27 23:35:41 +0000

    copy/understand/isolate the click function

--- projects/personality-quiz/public/scripts/script.js
+++ projects/personality-quiz/public/scripts/script.js
@@ -3,7 +3,7 @@
 
 // Create a paragraph element
 const p = document.createElement('p');
-const p2 = document.createElement('p');
+const p2 = document.createElement('h1');
 
 // Create the text of the paragraph.
 const text = document.createTextNode('This paragraph is created dynamically.');
@@ -20,7 +20,7 @@ document.querySelector('body').append(p);
 // Add ten paragraphs.
 
 // Do the same thing as before but in a loop
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 11; i++) {
   const p = document.createElement('p');
   p.append(document.createTextNode(`Counting ${i}.`));
   document.querySelector('body').append(p);
@@ -30,6 +30,7 @@ for (let i = 0; i < 10; i++) {
 // This function is an event handler. (Similar to the functions we used with
 // registerOnclick in some of our old environments.)
 
+//when the click happens everything in this function will run
 const recordClick = (e) => {
   // Create a paragraph element.
   const p = document.createElement('p');
@@ -42,21 +43,38 @@ const recordClick = (e) => {
 
   // Add the paragraph to the body of the document
   document.getElementById('clicks').append(p);
+
+
 };
+
+const recordClick2 = (e) => {
+  const p2 = document.createElement('p');
+
+  p2.append(document.createTextNode('foobar'))
+
+  p2.append(p2)
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Add something to every existing paragraph.
 
+const c = document.createElement('h1');
+const b = document.createElement('b');
+
 document.querySelectorAll('p').forEach((p) => {
   // Create a bold element.
-  const b = document.createElement('b');
+
 
   // Create some text and add it to the bold element.
   b.append(document.createTextNode('Click me!'));
+  c.append(document.createTextNode('click me'));
 
   // Register the recordClick function to handle when the bold element is
   // clicked.
   b.onclick = recordClick;
+
+  c.onclick = recordClick2;
 
   // Append some text to the current paragraph.
   p.append(document.createTextNode(' '));
@@ -64,3 +82,4 @@ document.querySelectorAll('p').forEach((p) => {
   // And finally append the bold element to the paragraph.
   p.append(b);
 });
+
