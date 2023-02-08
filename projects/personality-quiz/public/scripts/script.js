06f1412b41009e1b698b9b6813b9a03400ce61f5
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-08 22:54:57 +0000

    combining the record answer functi

--- projects/personality-quiz/public/scripts/script.js
+++ projects/personality-quiz/public/scripts/script.js
@@ -1,14 +1,15 @@
 //DEFINING THE VARIABLES
 let amountOfAnswers = 0
 let currentQuestion = 0
+let score = 0
 const questions = [
-  { charge: +, question: 'QUESTION 1', influence: 3 },
+  { charge: 'positive', question: 'QUESTION 1', influence: 3 },
   { charge: 'negative', question: 'QUESTION 2', influence: 1 }]
 
 //ASSIGNING NAMES TO ELEMENTS
-const answer = document.createElement('p');
-const answer2 = document.createElement('p');
-const question = document.createElement('h1');
+const question = document.getElementById('question');
+const yes = document.getElementById('yes');
+const no = document.getElementById('no');
 
 //SEQUENCE QUESTION SELECTOR
 
@@ -19,9 +20,6 @@ const sequenceQuestionSelector = () => {
 //DISPLAY ANSWER
 
 const displayAnswer = (score) => {
-  answer.replaceChildren('')
-  answer2.replaceChildren('')
-  question.replaceChildren('')
   if (score > 0) {
     question.replaceChildren('YOU ARE RESULT 1')
   }
@@ -37,18 +35,18 @@ const displayAnswer = (score) => {
 //RECORD ANSWER FUNCTIONS
 const recordAnswer = (e) => {
   console.log('foobar')
-  console.log(e.currentTarget)
-  if (e.currentTarget === answer) {
-    if (questions[currentQuestion] === 'positive') {
+  //console.log(e.currentTarget)
+  if (e.currentTarget === 'yes') {
+    if (questions[currentQuestion].charge === 'positive') {
       score = score + questions[currentQuestion].influence
     }
     else if (questions[currentQuestion] === 'negative') {
-      score = score - questions[currentQuestion.influence]
+      score = score - questions[currentQuestion].influence
     }
   }
   else if (e.currentTarget === answer2) {
     if (questions[currentQuestion] === 'negative') {
-      score = score - questions[currentQuestion.influence]
+      score = score - questions[currentQuestion].influence
     }
     else if (questions[currentQuestion] === 'positive') {
       score = score + questions[currentQuestion.influence]
@@ -57,21 +55,10 @@ const recordAnswer = (e) => {
   amountOfAnswers++
   question.replaceChildren(sequenceQuestionSelector().question);
   if (amountOfAnswers >= questions.length) {
-    displayAnswer(score);
+    displayAnswer(score)
   }
 }
 
-//MAKING THE TEXT NODES
-answer.append('YES');
-answer2.append('NO');
-question.append('Select answer to begin')
-
 //ASSIGNING CLICKING ELEMENTS TO FUNCTIONS
-answer.onclick = recordAnswer
-answer2.onclick = recordAnswer
-
-//APPENDING ELEMENTS TO THE BODY
-const body = document.body;
-body.append(question);
-body.append(answer);
-body.append(answer2);
+yes.onclick = recordAnswer
+no.onclick = recordAnswer
