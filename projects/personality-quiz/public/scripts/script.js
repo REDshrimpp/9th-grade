4c83d961854726bbca24b462bf63e78ba4419a52
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-09 23:31:44 +0000

    adding questions

--- projects/personality-quiz/public/scripts/script.js
+++ projects/personality-quiz/public/scripts/script.js
@@ -5,18 +5,18 @@ let score = 0
 const questions = [
   { charge: 'negative', question: 'do you poor the milk first?', influence: 5 },
   { charge: 'negative', question: 'do you like the sun?', influence: 1 },
-  { charge: 'negative', question: 'do snakes intimidate you?', influence: 3 },
+  { charge: 'negative', question: 'do snakes intimidate you?', influence: 6 },
   { charge: 'positive', question: 'thoughts on boulders?', influence: 1 },
-  { charge: 'positive', question: 'tu tienes que bebir la sandia?', influence: 4 },
-  { charge: 'positive', question: '', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },
-  { charge: 'positive', question: 'QUESTION 2', influence: 1 },]
+  { charge: 'positive', question: 'tu tienes que bebir la jugo sandia?', influence: 4 },
+  { charge: 'positive', question: 'have you ever broken any bones?', influence: 3 },
+  { charge: 'negaive', question: 'do you like beige?', influence: 1 },
+  { charge: 'positive', question: 'cats or dogs', influence: 1 },
+  { charge: 'positive', question: 'do you like hoodies?', influence: 4 },
+  { charge: 'negative', question: 'are you a morning person', influence: 3 },
+  { charge: 'negative', question: 'dont you not own an air fryer?', influence: 2 },
+  { charge: 'negative', question: 'would you be scared to pet a spikey lizard?', influence: 4 },
+  { charge: 'positive', question: 'is long hair unnatractive?', influence: 7 },
+  { charge: 'positive', question: 'is the ocean annoying?', influence: 4 },]
 
 
 //ASSIGNING NAMES TO ELEMENTS
@@ -34,10 +34,10 @@ const sequenceQuestionSelector = () => {
 
 const displayAnswer = (score) => {
   if (score > 0) {
-    question.replaceChildren('you are right')
+    question.replaceChildren('you passed')
   }
   else if (score < 0) {
-    question.replaceChildren('you are left')
+    question.replaceChildren('you didnt pass')
   }
 
   else {
