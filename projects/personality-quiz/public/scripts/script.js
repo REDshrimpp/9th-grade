7efcc537b124a567c42123bf6f42c299e5fff165
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-10 23:35:37 +0000

    making the questions random and not repeating

--- projects/personality-quiz/public/scripts/script.js
+++ projects/personality-quiz/public/scripts/script.js
@@ -3,20 +3,20 @@ let amountOfAnswers = 0
 let currentQuestion = 0
 let score = 0
 const questions = [
-  { charge: 'negative', question: 'do you poor the milk first?', influence: 5 },
-  { charge: 'negative', question: 'do you like the sun?', influence: 1 },
-  { charge: 'negative', question: 'do snakes intimidate you?', influence: 6 },
-  { charge: 'positive', question: 'thoughts on boulders?', influence: 1 },
-  { charge: 'positive', question: 'tu tienes que bebir la jugo sandia?', influence: 4 },
-  { charge: 'positive', question: 'have you ever broken any bones?', influence: 3 },
-  { charge: 'negaive', question: 'do you like beige?', influence: 1 },
-  { charge: 'positive', question: 'cats or dogs', influence: 1 },
-  { charge: 'positive', question: 'do you like hoodies?', influence: 4 },
-  { charge: 'negative', question: 'are you a morning person', influence: 3 },
-  { charge: 'negative', question: 'dont you not own an air fryer?', influence: 2 },
-  { charge: 'negative', question: 'would you be scared to pet a spikey lizard?', influence: 4 },
-  { charge: 'positive', question: 'is long hair unnatractive?', influence: 7 },
-  { charge: 'positive', question: 'is the ocean annoying?', influence: 4 },]
+  { charge: 'negative', question: 'do you pour the milk first?', influence: 5, asked: 'no' },
+  { charge: 'negative', question: 'do you like the sun?', influence: 1, asked: 'no' },
+  { charge: 'negative', question: 'do snakes intimidate you?', influence: 6 , asked: 'no' },
+  { charge: 'positive', question: 'thoughts on boulders?', influence: 1 , asked: 'no' },
+  { charge: 'positive', question: 'tu tienes que bebir la jugo sandia?', influence: 4 , asked: 'no' },
+  { charge: 'positive', question: 'have you ever broken any bones?', influence: 3 , asked: 'no' },
+  { charge: 'negaive', question: 'do you like beige?', influence: 1 , asked: 'no' },
+  { charge: 'positive', question: 'cats or dogs', influence: 1 , asked: 'no' },
+  { charge: 'positive', question: 'do you like hoodies?', influence: 4 , asked: 'no' },
+  { charge: 'negative', question: 'are you a morning person', influence: 3 , asked: 'no' },
+  { charge: 'negative', question: 'dont you not own an air fryer?', influence: 2 , asked: 'no' },
+  { charge: 'negative', question: 'would you be scared to pet a spikey lizard?', influence: 4 , asked: 'no' },
+  { charge: 'positive', question: 'is long hair unnatractive?', influence: 7 , asked: 'no' },
+  { charge: 'positive', question: 'is the ocean annoying?', influence: 4 , asked: 'no' },]
 
 
 //ASSIGNING NAMES TO ELEMENTS
@@ -45,29 +45,41 @@ const displayAnswer = (score) => {
   }
 }
 
+//RANDOM QUESTION FUNCTION
+
+const randomQuestion = () => {
+  let selectedQuestion = questions[Math.floor(Math.random() * questions.length) + 1]
+  while (selectedQuestion.asked !== 'no') {
+    selectedQuestion = questions[Math.floor(Math.random() * questions.length) + 1]
+  }
+  return selectedQuestion
+}
+
 //RECORD ANSWER FUNCTIONS
 const recordAnswer = (e) => {
-  amountOfAnswers++;
   if (amountOfAnswers >= questions.length) {
     displayAnswer(score);
   }
+  let selectedQuestion = randomQuestion();
+  selectedQuestion.asked = 'yes'
+  amountOfAnswers++;
   if (e.currentTarget === yes) {
-    if (questions[currentQuestion].charge === 'positive') {
+    if (selectedQuestion.charge === 'positive') {
       score = score + questions[currentQuestion].influence
     }
-    else if (questions[currentQuestion].charge === 'negative') {
+    else if (selectedQuestion.charge === 'negative') {
       score = score - questions[currentQuestion].influence
     }
   }
   else if (e.currentTarget === no) {
-    if (questions[currentQuestion].charge === 'negative') {
+    if (selectedQuestion.charge === 'negative') {
       score = score + questions[currentQuestion].influence
     }
-    else if (questions[currentQuestion].charge === 'positive') {
+    else if (selectedQuestion.charge === 'positive') {
       score = score - questions[currentQuestion].influence
     }
   }
-  question.replaceChildren(sequenceQuestionSelector().question);
+  question.replaceChildren(selectedQuestion.question);
 }
 
 //ASSIGNING CLICKING ELEMENTS TO FUNCTIONS
