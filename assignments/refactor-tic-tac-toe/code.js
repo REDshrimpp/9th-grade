2d2223124a1fa5c782bdee860df798b251629987
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-22 15:20:11 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -90,6 +90,8 @@ const isWinnerAlready = () => {
 }
 
 const isLegalMove = (x, y) => {
+  let r = Math.floor((y - boardTop) / cellSize);
+  let c = Math.floor((x - boardLeft) / cellSize);
   return 0 <= r && r < 3 && 0 <= c && c < 3  && board[r][c] === ''
 }
 
