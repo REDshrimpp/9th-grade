87685af4e04268071c52ce2c33f0bcd5988abc61
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-22 15:00:45 -0800

    Updating assignments/refactor-tic-tac-toe/code.js

--- assignments/refactor-tic-tac-toe/code.js
+++ assignments/refactor-tic-tac-toe/code.js
@@ -104,6 +104,7 @@ registerOnclick((x, y) => {
   // Only do anything if it's a legal move and the game isn't over.
   if (!isWinnerAlready()) {
 
+console.log('test')
     // Draw the mark and record the move
     const marker = move % 2 === 0 ? 'X' : 'O';
     const x = boardLeft + c * cellSize + cellSize / 2;
