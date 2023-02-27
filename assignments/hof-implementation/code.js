b5144a3d5df3b7bc0287d58a932ea51612a239f8
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-27 15:23:32 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -1,6 +1,13 @@
 // N.B. Do not use the array methods of the same name to implement these functions!
 
 const filter = (predicate, array) => {
+  let newArray = []
+  for(let x = 0; x < array.length; x++) {
+    if(array[x] === predicate) {
+      newArray.push(array[x])
+    }
+  }
+  return newArray
 };
 
 const map = (fn, array) => {
