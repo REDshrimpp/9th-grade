f23b96c98ada12757c793c5d4b9ed1258f02740a
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-27 15:24:42 -0800

    Updating assignments/hof-implementation/code.js

--- assignments/hof-implementation/code.js
+++ assignments/hof-implementation/code.js
@@ -2,8 +2,8 @@
 
 const filter = (predicate, array) => {
   let newArray = []
-  for(let x = 0; x < array.length; x++) {
-    if(array[x] === predicate) {
+  for (let x = 0; x < array.length; x++) {
+    if (array[x] === predicate) {
       newArray.push(array[x])
     }
   }
