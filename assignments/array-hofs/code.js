89b3aa16b96e9a950e95703da475d13ce7f64794
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-02-28 15:19:50 -0800

    Updating assignments/array-hofs/code.js

--- assignments/array-hofs/code.js
+++ assignments/array-hofs/code.js
@@ -1,5 +1,25 @@
 //EVENS
 
 const evens = (array) => {
-  array.filter( % 2 === 0)
+  return array.filter(n % 2 === 0)
+}
+
+//odds
+const odds = (array) => {
+  return array.filter(n & 2 !== 0)
+}
+
+//big
+const big = (array) => {
+  return array.filter(n > 100)
+}
+
+//grades
+const grades = (array) => {
+  return array.filter(n.grade !== undefined)
+}
+
+//pairs
+const pairs = (array) => {
+  return array.flatmap(n = n, n)
 }
