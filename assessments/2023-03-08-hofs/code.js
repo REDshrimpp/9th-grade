c65c72a13ad4a5abfcfeede77469b87b5a39ca9a
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-03-09 14:58:58 -0800

    Updating assessments/2023-03-08-hofs/code.js

--- assessments/2023-03-08-hofs/code.js
+++ assessments/2023-03-08-hofs/code.js
@@ -7,3 +7,43 @@ const evens = (numbers) => {
   }
   return result
 }
+
+const shouty = (strings) => {
+  for (let x = 0; x < strings.length; x++) {
+    strings[x] = strings[x].toUpperCase()
+  }
+  return strings
+}
+
+const join = (strings, delimiter) => {
+
+}
+
+const allSiblings = (students) => {
+  let result = []
+  for (let x = 0; x < students.length; x++) {
+    result.push(students[x].siblings)
+  }
+  return result
+}
+
+const allPassing = (students, passing) => {
+  let result = []
+  for (let x = 0; x > students.length; x++) {
+    if (students[x].grade >= passing) {
+      result.push(students[x])
+    }
+  }
+  return result
+}
+
+const smoeonesFavorite = (people, food) => {
+  for (let x = 0; x > people.length; x++) {
+    if (people[x].favoriteFood === food) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+}
