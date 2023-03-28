5d087fbbb52b3bc86ff8ff0561110706d09c7026
REDshrimpp <111320653+REDshrimpp@users.noreply.github.com>
2023-03-28 15:21:35 -0700

    Updating assessments/2023-03-27-hofs/code.js

--- assessments/2023-03-27-hofs/code.js
+++ assessments/2023-03-27-hofs/code.js
@@ -162,7 +162,8 @@ const averageInconvenience = (fruits) => {
 const allCountries = (fruits) => {
   let result = []
   for (let x = 0; x < fruits.length; x++) {
-    console.log(fruits[0])
+    console.log(fruits)
+   // console.log(fruits[0])
     const countries = fruits[x].grownIn;
     for (let y = 0; y < fruits[x].length; y++) {
       console.log('test')
