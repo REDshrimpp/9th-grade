98cc89eb0e095e5f548c92df720e657264bb90ce
brooks <111320653+REDshrimpp@users.noreply.github.com>
2023-04-10 15:25:05 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -3,6 +3,15 @@ const factorial = (n) => {
     return 1
   }
   else {
-    return n + factorial(n - 1)
+    return n * factorial(n - 1)
+  }
+}
+
+const triangular = (n) => {
+  if (n === 0) {
+    return 1
+  }
+  else {
+    return n + factorial(n -1)
   }
 }
