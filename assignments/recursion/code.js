8d737138279c6188a1080266528ef17470ff3880
brooks <111320653+REDshrimpp@users.noreply.github.com>
2023-04-17 15:02:01 -0700

    Updating assignments/recursion/code.js

--- assignments/recursion/code.js
+++ assignments/recursion/code.js
@@ -35,10 +35,7 @@ const gcd = (a, b) => {
 }
 
 const sum = (array) => {
-  if (array.length === 0) {
-    return 0
-  }
-  else if (array.length === 1) {
+  if (array.length === 1) {
     return array[0]
   }
   else {
